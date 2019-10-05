c3487a55455282c30d1fb58136d9d090d04dec69
veotani <veotani@localhost.localdomain>
2019-10-05 17:02:25 +0300

    remove test for docx as it's invalid

--- parsers/docx/parser_test.go
+++ parsers/docx/parser_test.go
@@ -1,19 +1,1 @@
 package docx
-
-import (
-	"strings"
-	"testing"
-)
-
-func TestParseEmptyFile(t *testing.T) {
-	parser := Parser{"../data/empty.docx"}
-	text, err := parser.GetText()
-	if err != nil {
-		t.Error(err)
-	}
-
-	mustBeEmpty := strings.ReplaceAll(text, " ", "")
-	if mustBeEmpty != "" {
-		t.Errorf("after replacing all the whitespaces content had to be empty, but it was:\n %v", mustBeEmpty)
-	}
-}
